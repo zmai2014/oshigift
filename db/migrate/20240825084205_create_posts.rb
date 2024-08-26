9e61be3c01b1f717e82f1b13429f4990e3da6301
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-08-26 14:29:23 +0000

    change top and post table

--- db/migrate/20240825084205_create_posts.rb
+++ db/migrate/20240825084205_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :item_name
-      t.text :introduction
+      t.string :item_name, null: false
+      t.text :introduction, null: false
       t.integer :age, defalut: 0
       t.integer :gender, defalut: 0
       t.integer :price, defalut: 0
