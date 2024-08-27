acea8124dccdeac0d2eb67aeeedb15b6fef40755
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-08-27 14:54:27 +0000

    make mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  def mypage
+  end
+  
   def show
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :users
   resources :posts
+  get 'mypage' => 'users#mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
