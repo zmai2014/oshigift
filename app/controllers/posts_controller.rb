4ca8f4d231898ca530a9bdcc9ef5769f7c17b1e3
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-09-04 11:21:21 +0000

    add post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to post_path(post.id)
+      redirect_to post_path(@post.id)
     else
       render :new
     end
